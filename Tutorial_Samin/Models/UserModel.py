2493b9ef70d4fbd13527c52fc1a88dce5fb1461f
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2015-12-27 21:08:28 +0330

    Update UserModel.py

--- Tutorial_Samin/Models/UserModel.py
+++ Tutorial_Samin/Models/UserModel.py
@@ -16,9 +16,61 @@ class Student(Base):
         fullname = Column(String(50))
         password = Column(String(50))
 
-        '''
-        def __repr__(self):
-            return "<Student(name='%s', fullname='%s', password='%s')>" % (self.name, self.fullname, self.password)
+# OneToMany
+class Parent(Base):
+    __tablename__ = 'parent'
+    id = Column(Integer, primary_key=True)
+    children = relationship("Child")
 
-        '''
+class Child(Base):
+    __tablename__ = 'child'
+    id = Column(Integer, primary_key=True)
+    parent_id = Column(Integer, ForeignKey('parent.id'))
+
+#ManyToOne
+
+class Parent(Base):
+    __tablename__ = 'parent'
+    id = Column(Integer, primary_key=True)
+    child_id = Column(Integer, ForeignKey('child.id'))
+    child = relationship("Child", back_populates="parents")
+
+class Child(Base):
+    __tablename__ = 'child'
+    id = Column(Integer, primary_key=True)
+    parents = relationship("Parent", back_populates="child")
+
+
+#OneToOne
+
+class Parent(Base):
+    __tablename__ = 'parent'
+    id = Column(Integer, primary_key=True)
+    child = relationship("Child", uselist=False, back_populates="parent")
+
+class Child(Base):
+    __tablename__ = 'child'
+    id = Column(Integer, primary_key=True)
+    parent_id = Column(Integer, ForeignKey('parent.id'))
+    parent = relationship("Child", back_populates="child")
+
+#ManyToMany
+
+'''
+association_table = Table('association', Base.metadata,
+    Column('left_id', Integer, ForeignKey('left.id')),
+    Column('right_id', Integer, ForeignKey('right.id'))
+)
+
+class Parent(Base):
+    __tablename__ = 'left'
+    id = Column(Integer, primary_key=True)
+    children = relationship("Child",
+                    secondary=association_table)
+
+class Child(Base):
+    __tablename__ = 'right'
+    id = Column(Integer, primary_key=True)
+'''
+
 
