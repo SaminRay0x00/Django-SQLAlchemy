c8e905f8851919d68a7831ab4893424680f4db0d
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-01-15 09:21:17 +0330

    Update self-relationship.py

--- Tutorial_Samin/Models/self-relationship.py
+++ Tutorial_Samin/Models/self-relationship.py
@@ -1,1 +1,21 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String ,ForeignKey
+from sqlalchemy.orm import relationships
+from sqlalchemy.orm import relationship
+Base = declarative_base()
 
+
+
+class Student(Base):
+
+        __tablename__ = 'reltest'
+
+        id = Column(Integer, primary_key=True)
+        name = Column(String(50))
+        fullname = Column(String(50))
+        password = Column(String(50))
+        parent_id = Column(Integer, ForeignKey('reltest.id'))
+        children = relationship("Student",lazy="joined",join_depth=2)
+
+from Models.ConnectorDB import ConnectEngine
+Base.metadata.create_all(ConnectEngine().engine)
