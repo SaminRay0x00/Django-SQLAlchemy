fe7706e8a8b048d748c9961f6fa5a0e098a4e3a1
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-01-15 09:45:11 +0330

    Update ChildModel.py
    relationship One2May  (ParentModel.py/ChildModel.py)

--- Tutorial_Samin/Models/ChildModel.py
+++ Tutorial_Samin/Models/ChildModel.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Column, Integer, String ,ForeignKey
+from Models.UserModel import Base
+from sqlalchemy.orm import relationships
+from sqlalchemy.orm import relationship
+#Base = declarative_base()
+
+class Child(Base):
+    __tablename__ = 'child_test'
+    id = Column(Integer, primary_key=True)
+    parent_id = Column(Integer, ForeignKey('parent_test.id'))
 
