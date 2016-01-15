37bbac1d21d8f983b972c454241156ef8ac84d24
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-01-15 09:43:50 +0330

    Update ParentModel.py

--- Tutorial_Samin/Models/ParentModel.py
+++ Tutorial_Samin/Models/ParentModel.py
@@ -1,1 +1,17 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String ,ForeignKey
+from sqlalchemy.orm import relationships
+from sqlalchemy.orm import relationship
+
+
+# very important for relationship(Parent/Child)
+Base = declarative_base()
+
+
+class Parent(Base):
+
+    from Models.Child import Child
+    __tablename__ = 'parent_test'
+    id = Column(Integer, primary_key=True)
+    children = relationship("Child")
 
