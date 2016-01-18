e89fb732cb7db023611b818de888570127293f0f
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-01-18 08:36:51 +0330

    Update UserBL.py

--- Tutorial_Samin/BL/UserBL.py
+++ Tutorial_Samin/BL/UserBL.py
@@ -62,8 +62,37 @@ def Transaction():
     except:
         trans.rollback()
         raise
-      
-      
+
+
+### EXEC SP 
+def exec_procedure(session, proc_name, params):
+
+
+    sql_params = ",".join(["@{0}={1}".format(name, value) for name, value in params.items()])
+    sql_string = """
+        DECLARE @return_value int;
+        EXEC    @return_value = [dbo].[{proc_name}] {params};
+        SELECT 'Return Value' = @return_value;
+    """.format(proc_name=proc_name, params=sql_params)
+
+    return session.execute(sql_string).fetchall()
+
+
+### Call SP 
+
+def call_procedure():
+
+
+
+       Session = sessionmaker(bind=ConnectEngine())
+       sessiondb = Session()
+
+       params = {
+                    'Foo': 1,
+                    'Bar': 1   ,
+                 }
+       exec_procedure(sessiondb, 'MyProc', params)
+       
 def SelectOne():
                 pass
               
