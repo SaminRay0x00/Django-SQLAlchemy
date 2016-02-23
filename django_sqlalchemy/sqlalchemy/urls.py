46e2e9d121fc2a403ee76a699bf113dd9b21d6d7
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-02-23 08:51:28 +0330

    django sqlalchemy orm

---
+++ django_sqlalchemy/sqlalchemy/urls.py
@@ -0,0 +1,32 @@
+"""Tutorial_Samin URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', "saminappp.views.index" , name='index'),
+    url(r'^contact$', "saminappp.views.contact" , name='contact'),
+    url(r'^about$', "saminappp.views.about" , name='about'),
+    url(r'^home', "saminappp.views.home" , name='home'),
+    url(r'^insert', "UI.StudentView.Insert" , name='Insert'),
+    url(r'^select', "UI.StudentView.SelectAll" , name='select'),
+
+
+
+
+]
