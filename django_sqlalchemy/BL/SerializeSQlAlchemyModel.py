a6152c9e4a7a4ee80aaf0541dc83cc644dc6fd12
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-02-24 10:31:40 +0330

    Change method name connector DB

--- django_sqlalchemy/BL/SerializeSQlAlchemyModel.py
+++ django_sqlalchemy/BL/SerializeSQlAlchemyModel.py
@@ -1,17 +1,18 @@
 from sqlalchemy.orm import class_mapper
 from json import dumps
+from Models.ConnectorDB import ConnectMySQL
 
 class serialize_sqlalchemy(object):
 
 	def serialize(self,model):
 
-	    self.columns = [c.key for c in class_mapper(model.__class__).columns]
+			self.columns = [c.key for c in class_mapper(model.__class__).columns]
     	    return dict((c, getattr(model, c)) for c in self.columns)
 
 
 	def SelectAll(self):
 
-                        Session = sessionmaker(bind=ConnectEngine())
+                        Session = sessionmaker(bind=ConnectMySQL())
                         self.sessiondb = Session()
                         self.serialized_labels = [serialize(label) for label in sessiondb.query(Student).all()]
                         self.my_json = dumps(self.serialized_labels)
